c26563870e8e10cd3900eda03ca875c7b1178b71
PrivateAnariel <bruno-gosselin@live.ca>
2015-03-09 14:16:47 -0400

    Debut ajout modification pwd

--- FoodOrdr/src/AppBundle/Controller/DefaultController.php
+++ FoodOrdr/src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+
 class DefaultController extends Controller
 {
     /**
@@ -17,6 +20,36 @@ class DefaultController extends Controller
 		$clients = $clientRepository->findAll();
 		$name = $clients[0]->getCourriel()." ".$clients[0]->getMdp();
 		$params = array("hello" => $name);
+        return $this->render('default/index.html.twig', $params);
+    }
+    /**
+     * @Route("/NouveauMdp", name="client_mdp")
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function newMdpAction() {
+        $data = array();
+        $form = $this->createFormBuilder($data)
+            ->add('mdp', 'password', array(
+                'constraints' => array(
+                    new NotBlank(),
+                    new UserPassword(),
+                ),
+            ))
+            ->add('nouveauMdp', 'repeated', array(
+               'first_name' => 'nouveau_mdp',
+               'second_name' => 'confirm_mdp',
+               'type' => 'password',
+               'constraints' => array(
+                    new NotBlank(),
+                ),
+            ))
+            ->getForm();
+        $form->handleRequest($this->getRequest());
+        if ($form->isValid()) {
+            
+            $data = $form->getData();
+        }
+        $params = array("hello" => "test");
         return $this->render('default/index.html.twig', $params);
     }
 }
